a475e57b2230c29dda8485667cbf2ea602183011
Scott Baggett <hello@scottbaggett.com>
2015-09-02 15:34:42 -0700

    manifest assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Only include gem installed javascript assets here. All other js develoment should use Common JS
-// and will be compiled with Gulp and Browserify
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//
-// Pull compiled from /public/assets/javascripts
-//= require global
+/**
+ * This is the main site manifest.
+ * Require any gem based requirements here.
+ *
+ *
+**/
+
+/**
+ * For NPM based requirements, use WebPack to create bundles.
+ *
+ * = require main
+**/
