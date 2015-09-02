ceaa44e9ba032b2c2a795238f0400a2e514e7acb
Scott Baggett <hello@scottbaggett.com>
2015-09-02 16:25:30 -0700

    html cleanup removed

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,5 +1,4 @@
 var gulp      = require('gulp');
-var html      = require('../config/html');
 var iconFont  = require('../config/iconFont');
 var svgSprite = require('../config/svg-sprite');
 var images    = require('../config/images');
@@ -13,5 +12,4 @@ gulp.task('watch', ['browserSync'], function() {
   watch(iconFont.src, function() { gulp.start('iconFont'); });
   watch(svgSprite.src, function() { gulp.start('svg-sprite'); });
   watch(fonts.src, function() { gulp.start('fonts'); });
-  watch(html.watch, function() { gulp.start('html'); });
 });
