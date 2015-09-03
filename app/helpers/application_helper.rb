6cd64f29483e22deff8fa24f7cf10ec52a36a0a3
Scott Baggett <hello@scottbaggett.com>
2015-09-02 17:03:41 -0700

    svg sprites

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     path = REV_MANIFEST[path] if defined?(REV_MANIFEST)
     "/assets/#{path}"
   end
+
+  ## Render a sprite from the sprite sheet.
+  def svgsprite(args)
+    svg_class = args[:class] || ""
+    """
+    <svg class=\"#{svg_class}\">
+      <use xlink:href=\"/assets/images/spritesheets/sprites.svg##{args[:id]}\"></use>
+    </svg>
+    """
+  end
 end
